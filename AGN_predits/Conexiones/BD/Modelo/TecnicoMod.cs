a6b2e3e25600402a65504d8ee925c4b88bdca64a
BrunoDepetris <80788890+Bruno-Depetris@users.noreply.github.com>
2026-02-24 15:49:43 -0800

    cc

---
+++ AGN_predits/Conexiones/BD/Modelo/TecnicoMod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGN_predits.Conexiones.BD.Modelo
+{
+    internal class TecnicoMod
+    {
+        public int TecnicoID { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public decimal Bateria { get; set; }
+        public string Falla { get; set; }
+        public string Email { get; set; }
+        public decimal Almacenamiento { get; set; }
+        public decimal CostoArreglo { get; set; }
+        public decimal Costo { get; set; }
+    }
+}
